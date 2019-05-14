cd7c93b3293b04496ed56ebb16a2c254bc2d508f
Yasmin Rizky Fairuza <yasmin_rizky@apps.ipb.ac.id>
2019-05-14 10:59:04 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,9 +3,9 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------
     | Default Database Connection Name
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------
     |
     | Here you may specify which of the database connections below you wish
     | to use as your default connection for all database work. Of course
@@ -16,9 +16,9 @@ return [
     'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------
     | Database Connections
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------
     |
     | Here are each of the database connections setup for your application.
     | Of course, examples of configuring each database platform that is
